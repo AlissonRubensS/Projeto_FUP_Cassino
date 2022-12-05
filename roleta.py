0c171d8dfd6023b6a92ebc724c70294749ba0703
Komeiji <113029963+KoishiKomeiji213@users.noreply.github.com>
2022-12-05 11:47:39 -0300

    Add files via upload

---
+++ roleta.py
@@ -0,0 +1,180 @@
+from random import randint
+
+Black = [4,2,15,11,17,6,10,13,20,22,8,33,26,24,31,35,28,29]
+Red = [1,3,18,7,5,21,9,16,24,14,12,26,30,27,19,32,23,25]
+
+mult = 0
+Coins = 1000
+
+   
+Bet = {
+    'BetType':'none',
+    'BetValue': 0,
+      }
+    
+
+def BetTypeColors(BetCoins, Color): # aposta por cores 2x    
+    
+    if BetCoins > 0 and BetCoins <= Coins: # checa se a aposta é valida
+        Bet['BetValue'] = BetCoins #define o valor da aposta e joga para o dicionario
+    
+        if Color == 'Red': #checa a cor apostada
+            Bet.update({'Color':'Red'}) #joga a cor para o dicionario
+            ColorsCheck() #chama a função de win condition
+    
+        elif Color == 'Black': #checa a cor apostada
+            Bet.update({'Color':'Black'}) #joga a cor para o dicionario
+            ColorsCheck() #chama a wincondition
+    
+        else:
+            aposta() #chama novamente a aposta
+
+def BetTypeEvenOrOdd(BetCoins, EvenOrOdd):  #chama a função da aposta de impar ou par
+    
+    if BetCoins > 0 and BetCoins <= Coins: #checa se a apostá é possivel
+        Bet['BetValue'] = BetCoins # define o valor da aposta no dicionario
+        
+        if EvenOrOdd == 'Even': #caso a aposta seja par
+            Bet.update({'EvenorOdd': 'Even'}) #adicina o tipo da aposta no dicionario
+            EvenOrOddCheck() #chama a wincondition
+       
+        elif EvenOrOdd == 'Odd': # caso a aposta seja impar, faz a mesma coisa
+            Bet.update({'EvenorOdd': 'Odd'})
+            EvenOrOddCheck()
+       
+        else:
+            aposta() #caso a aposta nãa seja definida chama a função de aposta novamente, para que seja resetado
+        
+def BetTypeDozen(BetCoins, Dozens): #DUZIAS
+    
+    if BetCoins > 0 and BetCoins <= Coins: #checa se a aposta é possivel
+        Bet['BetValue'] = BetCoins #define o valor da aposta no dicionario
+
+        if Dozens == 1: # caso a aposta seja feita no 1, ela vai fazer na primeira duzia 1 a 12
+            Bet.update({'Dozen': 'First'}) #adiciona a informação no dicionario
+            Dozencheck() #puxa a wincondition
+        
+        if Dozens == 2: # caso a aposta seja feita no 2, ela vai fazer na primeira duzia 13 a 24
+            Bet.update({'Dozen': 'Second'}) #adiciona a informação no dicionario
+            Dozencheck()#puxa a wincondition
+        
+        if Dozens == 3: # caso a aposta seja feita no 3, ela vai fazer na primeira duzia 25 a 36
+            Bet.update({'Dozen': 'Third'}) #adiciona a informação no dicionario
+            Dozencheck()#puxa a wincondition
+        
+        else:
+            aposta()#caso a aposta nãa seja definida chama a função de aposta novamente, para que seja resetado
+        
+    
+def ColorsCheck(): # wincondition das cores
+    
+    NumSort = randint(1,36) # sorteia os numeros
+    
+    if  Bet['Color'] == 'Red': #condicional para caso seja vermelho, basicamente checa a key colors
+        if NumSort in Red:     #checa se o numero selecionado tá dentro da lista dos vermelhos
+            Bet.update({'WinOrLose':'Win'}) #caso esteja ele adiciona o estado de vitoria
+
+        else: 
+            Bet.update({'WinOrLose':'Lose'}) # caso não esteja, ele adiciona a condiação como derrota.
+   
+    elif Bet['Color'] == 'Black':  # mesma coisa que o de cima
+        if NumSort in Black:
+            Bet.update({'WinOrLose':'Win'})
+
+        else:
+            Bet.update({'WinOrLose':'Lose'})
+
+    
+def EvenOrOddCheck(): # wincondition do impar ou par
+
+    NumSort = randint(1,36) #sorteia o numero
+    
+    if Bet['EvenorOdd'] == 'Even': # caso a aposta seja par
+        if NumSort % 2 == 0: #checa se o numero é par
+                   
+            Bet.update({'WinOrLose':'Win'}) #se sim, ele adiciona a condição de vitoria
+
+        else:
+            Bet.update({'WinOrLose':'Lose'}) # caso não, ele adiciona a de derrota
+    
+    elif Bet['EvenorOdd'] == 'Odd': #o mesmo acontece com o impar
+        if NumSort %2 != 0:
+        
+            Bet.update({'WinOrLose': 'Win'})
+        
+        else:
+            Bet.update({'WinOrLose':'Lose'})
+  
+def Dozencheck(): #wincondition das duzias
+    
+    NumSort = randint(1,36) #sorteia  o numero
+    print(NumSort)
+    if Bet['Dozen'] == 'First':
+        if 1 <= NumSort <= 12:
+            print('primeiro')
+            Bet.update({'WinOrLose':'Win'})
+            result()
+        else:
+            Bet.update({'WinOrLose':'Lose'})
+            result()
+    elif Bet['Dozen'] == 'Second':
+        
+        if 13 <= NumSort <= 24:
+            print('segundo')
+            Bet.update({'WinOrLose':'Win'})
+            result()
+        else:
+            Bet.update({'WinOrLose':'Lose'})
+            
+                
+    elif Bet['Dozen'] == 'Third':
+        if 24 < NumSort <= 36:
+            print('terceiro')
+            Bet.update({'WinOrLose':'Win'})
+
+        else:
+            Bet.update({'WinOrLose':'Lose'})   
+
+  
+
+def result():
+    global mult, Coins
+    
+    if Bet['WinOrLose'] == 'Win':
+        
+        if Bet['BetType'] == 'Colors':
+            mult = 2
+        
+        elif Bet['BetType'] == 'EvenOrOdd':
+            mult = 3
+        
+        elif Bet['BetType'] == 'Dozens':
+            print('vezes 5')
+            mult = 5
+        
+        Coins = Bet['BetValue'] * mult + Coins
+        print(Coins)
+    
+    elif Bet['WinOrLose'] == 'Lose':
+        
+        Coins = Coins - Bet['BetValue']
+        print(Coins)
+    
+def aposta():
+    BetType = int(input('a: '))
+
+    if BetType == 1:
+        Bet['BetType'] = 'Colors'
+        BetTypeColors(100, 'Black')
+    
+    elif BetType == 2:
+        Bet['BetType'] = 'EvenOrOdd'
+        BetTypeEvenOrOdd(100, 'Even')
+        
+    elif BetType == 3:
+        Bet['BetType'] = 'Dozens'
+        BetTypeDozen(100, 1)
+    
+while True:
+        aposta()
+        result()
